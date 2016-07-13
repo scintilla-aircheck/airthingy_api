26e4810ab212202940c782734418506dbbba9ced
Konrad R.K. Ludwig <konrad.rk.ludwig@gmail.com>
2016-07-13 08:52:33 -0700

    Generalize resource class for other models

---
+++ airthingy_api/resources.py
@@ -0,0 +1,61 @@
+import flask
+import flask_restful as frest
+
+
+class IModelResource(frest.Resource):
+    """
+    A base resource interface providing standard helper methods for working with
+    the Scintilla API.
+
+    By default, :class:`~IModelResource` will reject all methods with `405
+    METHOD NOT ALLOWED`.
+    """
+
+    # A data model for this resource
+    _MODEL = NotImplemented
+
+    # A dict of schemas for each method
+    _SCHEMAS = {
+        'POST': NotImplemented,
+        'GET': NotImplemented,
+        'PUT': NotImplemented,
+        'DELETE': NotImplemented
+    }
+
+    @property
+    def model(self):
+        """
+        Provides a read-only reference to the data model.
+        """
+        return self._MODEL
+    
+    @property
+    def schema(self):
+        return self._SCHEMAS[flask.request.method]
+
+    def get_cursor(self):
+        """
+        Instantiates a new table cursor object.
+        """
+        return self.model.select()
+
+    def get_params(self):
+        """
+        Returns validated request parameters based on the method type.
+        """
+        request = flask.request
+        raw_data = request.args if request.method == 'GET' \
+            else request.get_json()
+        return self.schema.load(raw_data)
+
+    def post(self):
+        flask.abort(405)
+
+    def get(self):
+        flask.abort(405)
+
+    def put(self):
+        flask.abort(405)
+
+    def delete(self):
+        flask.abort(405)
--- airthingy_api/sensors/resources.py
+++ airthingy_api/sensors/resources.py
@@ -1,7 +1,8 @@
 import flask
-import flask_restful as frest
 
 from playhouse.shortcuts import model_to_dict
+
+from airthingy_api import resources
 
 from . import schemas, models
 
@@ -9,28 +10,28 @@ from . import schemas, models
 _TARGETS = [t[0] for t in models.TARGETS]
 
 
-class SensorData(frest.Resource):
-    """`/api/{version}/data/"""
+class SensorData(resources.IModelResource):
 
     _MODEL = models.SensorDataPoint
 
-    @property
-    def _cursor(self):
-        return self._MODEL.select()
+    _SCHEMAS = {
+        'POST': schemas.CreateSensorData(),
+        'GET': schemas.GetSensorData()
+    }
 
     def post(self):
         """
         Create a new collection of sensor data points.
         """
-        data, errors = self.sanitize_data(schemas.CreateSensorData)
-        if errors:
-            flask.abort(400)
+        params = self.get_params()
+        if params.errors:
+            return params.errors, 400
         else:
-            data_points = data.pop('data')
+            data_points = params.data.pop('data')
             for data_point in data_points:
-                data_point['datetime'] = data['datetime']
-                data_point['latitude'] = data['latitude']
-                data_point['longitude'] = data['longitude']
+                data_point['datetime'] = params.data['datetime']
+                data_point['latitude'] = params.data['latitude']
+                data_point['longitude'] = params.data['longitude']
                 self._MODEL.create(**data_point)
             return None, 204
 
@@ -38,33 +39,16 @@ class SensorData(frest.Resource):
         """
         Query existing sensor data points.
         """
-        raw_data = flask.request.args
-        schema = schemas.GetSensorData()
-        schema_out = schema.load(raw_data)
-        if schema_out.errors:
+        params = self.get_params()
+        if params.errors:
             flask.abort(400)
 
-        target = schema_out.data.get('target')
+        cursor = self.get_cursor()
+        target = params.data.get('target')
         if target:
-            results = [model_to_dict(d) for d in self._cursor.where(self._MODEL.target == target)]
+            results = [model_to_dict(d) for d
+                       in cursor.where(self.model.target == target)]
         else:
-            results = [model_to_dict(d) for d in self._cursor]
+            results = [model_to_dict(d) for d in cursor]
 
         return flask.jsonify(results)
-
-    @staticmethod
-    def sanitize_data(schema, **kwargs):
-        """
-        Shortcut method to sanitize data with a given schema and return any
-        errors.
-
-        :param schema: A schema class
-        :return: A two-tuple containing data and form errors
-        """
-        if flask.request.method == 'GET':
-            raw_data = flask.request.args
-        else:
-            raw_data = flask.request.get_json()
-        schema = schema(**kwargs)
-        schema_output = schema.load(raw_data)
-        return schema_output.data, schema_output.errors
